e8a941c89c153d4c6ecfab7f5b9c2ca207cc0414
Kenneth Wilhelmsen <kenneth@wilhelmsen.nu>
2022-08-16 18:24:52 +0200

    Add ApplicationState

---
+++ src/main/java/services/m0b/m0bcraft/ApplicationState.java
@@ -0,0 +1,10 @@
+package services.m0b.m0bcraft;
+
+public class ApplicationState {
+
+    public ApplicationState() {
+        this.IsEventRelayEnabled = false;
+    }
+
+    public boolean IsEventRelayEnabled;
+}
